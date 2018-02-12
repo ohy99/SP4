2809bf0a13e3a49f3da7e2f736b77546f0f7a5b4
cjh2806 <cjh2806@gmail.com>
2018-02-12 15:24:28 +0800

    Health Bar
    Health Bar

---
+++ SP4/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    private float fillAmount;
+
+    private Player player;
+
+    private Image image;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player").GetComponent< Player >();
+        image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fillAmount = (float)player.GetHealth() / (float)player.GetMaxHealth() ;
+
+        image.fillAmount = fillAmount;
+	}
+}
---
+++ SP4/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private int score;
+    private int health;
+    private int maxHealth;
+
+    // Use this for initialization
+    void Start() {
+        score = PlayerPrefs.GetInt("Score", 0);
+        health = PlayerPrefs.GetInt("Health", 10);
+        maxHealth = PlayerPrefs.GetInt("MaxHealth", 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health < 0)
+        {
+            Save();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            health++;
+        }
+
+            if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            health--;
+        }
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetInt("Score", score);
+        PlayerPrefs.SetInt("Max Health", maxHealth);
+        PlayerPrefs.Save();
+
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public int GetMaxHealth()
+    {
+        return maxHealth;
+    }
+}
