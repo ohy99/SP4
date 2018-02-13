6a391e0b8d1621b17d680ec0fb33e7f5ecc2319b
ohy99 <ohy99@hotmail.com>
2018-02-13 11:19:45 +0800

    added Exp but no levelling

---
+++ SP4/Assets/Scripts/ExpSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpSpawner : MonoBehaviour {
+
+    int numOfActiveExp;
+    [SerializeField]
+    GameObject map;
+    [SerializeField]
+    GameObject expObj;
+
+    float elapsedTime;
+    [SerializeField]
+    float spawnDelay = 3.0f;
+    [SerializeField]
+    int maxSpawns = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= spawnDelay)
+        {
+            Vector3 pos = new Vector3(Random.Range(-map.transform.localScale.x * 0.5f, map.transform.localScale.x * 0.5f),
+            Random.Range(-map.transform.localScale.y * 0.5f, map.transform.localScale.y * 0.5f), 0 );
+            GameObject temp = Instantiate(expObj, pos, Quaternion.identity);
+            ++numOfActiveExp;
+            elapsedTime = 0.0f;
+        }
+	}
+
+    public void RemoveOne() { --numOfActiveExp; }
+}
