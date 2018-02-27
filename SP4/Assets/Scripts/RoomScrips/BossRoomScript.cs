fd52d4bebaf7c595352f6bfafc072b509b19e422
Samuel <bluecubestuff@gmail.com>
2018-02-27 09:57:10 +0800

    shift boss to spawn when player enter room
    sync boss finally done

--- SP4/Assets/Scripts/RoomScrips/BossRoomScript.cs
+++ SP4/Assets/Scripts/RoomScrips/BossRoomScript.cs
@@ -11,19 +11,66 @@ public class BossRoomScript : RoomScript {
     GameObject genericSpawner;
 
     GameObject[] bossList;
+    GameObject[] playersList;
+
+    bool isBossSpawn;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-    void Awake() {
-        Debug.Log("boss awkae");
-
+    void Awake()
+    {
+        isBossSpawn = false;
         //GameObject spawnBoss = Instantiate(boss, transform.position, Quaternion.identity);
         //spawnBoss.transform.parent = this.transform;
         //Global.Instance.boss = spawnBoss;
 
         //cahnge to be in update ltr
+        //if (Global.Instance.player.GetComponent<NetworkIdentity>().isServer)
+        //{
+        //    Debug.Log("Spwning da boss");
+        //    GameObject spawnBoss = genericSpawner.GetComponent<GenericSpawner>().SpawnObject(transform.position, boss);
+        //    spawnBoss.transform.parent = this.transform;
+        //    Global.Instance.boss = spawnBoss;
+        //}
+        //else
+        //{
+        //    Debug.Log("not server/host");
+        //    bossList = GameObject.FindGameObjectsWithTag("EnemyBoss");
+        //    Debug.Log("boss_array_length: " + bossList.Length);
+        //}   
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isBossSpawn)
+        {
+            //might puts this in global instead, but use it like this for now
+            playersList = GameObject.FindGameObjectsWithTag("Player");
+            for (int i = 0; i < playersList.Length; i++)
+            {
+                if (Vector3.Distance(playersList[i].transform.position, transform.position) < transform.localScale.x * 0.5f - 2.0f)
+                    SpawnBoss();
+            }
+        }
+
+        //boss not deaded
+        if(!Global.Instance.boss)
+        {
+            bossList = GameObject.FindGameObjectsWithTag("EnemyBoss");
+            if(bossList.Length > 0)
+            {
+                Global.Instance.boss = bossList[0];
+                Global.Instance.boss.transform.parent = this.transform;
+            }
+        }
+    }
+
+    void SpawnBoss()
+    {
         if (Global.Instance.player.GetComponent<NetworkIdentity>().isServer)
         {
             Debug.Log("Spwning da boss");
@@ -32,34 +79,11 @@ public class BossRoomScript : RoomScript {
             Global.Instance.boss = spawnBoss;
         }
         else
-        {
             Debug.Log("not server/host");
-            bossList = GameObject.FindGameObjectsWithTag("EnemyBoss");
-            Debug.Log(bossList.Length);
-        }   
+
+        //send a msg to say boss spawn
+        isBossSpawn = true;
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //might puts this in global instead, but use it like this for now
-        //playersList = GameObject.FindGameObjectsWithTag("Player");
-        //for (int i = 0; i < playersList.Length; i++)
-        //{
-        //    if (Vector3.Distance(playersList[i].transform.position, transform.position) < transform.localScale.x * 0.5f - 2.0f)
-        //    {
-        //        if (Global.Instance.player.GetComponent<NetworkIdentity>().isServer)
-        //        {
-        //            Debug.Log("Spwning da boss");
-        //            GameObject spawnBoss = genericSpawner.GetComponent<GenericSpawner>().SpawnObject(transform.position, boss);
-        //            spawnBoss.transform.parent = this.transform;
-        //            Global.Instance.boss = spawnBoss;
-        //        }
-        //        else
-        //            Debug.Log("not server/host");
-        //    }
-        //}
-
-    }
 }
